51f0e5c0a790c985a10070e522ca40b874fb776d
RAJA-NAGU <nagunglit@gmail.com>
2024-07-20 13:20:43 +0530

    Second Commit

--- assets/script/script.js
+++ assets/script/script.js
@@ -68,7 +68,7 @@ owl.owlCarousel({
   loop:true,
   // margin:10,
   autoplay:true,
-  autoplayTimeout:1000,
+  autoplayTimeout:2000,
   autoplayHoverPause:true,
   responsiveClass:true,
     responsive:{
